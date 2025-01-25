cf74e0f4e36107ced50d5e369b39e8fb985b783e
AaronStoor <Aaronbstoor@gmail.com>
2025-01-24 18:32:59 -0700

    completed rafting css file

--- Week 3/styles/styles.cs
+++ Week 3/styles/styles.cs
@@ -1,12 +1,2 @@
-body {
-    primary-color: #505033;
-    secondary-color: #264969;
-    accent1-color: #CFCFB4;
-    accent2-color: #B4CDE4;
-    font-family: "Roboto", serif;
-    font-weight: 400;
-}
 
-h1, h2, h3, h4, h5, h6 { 
-    font-weight: 400;
-}
+
